ea4f00c310e0d64de552597621510cc563af085e
bibekSpaceX <bibekkathariya1000@gmail.com>
2026-03-09 23:34:25 +0545

    UC2 Completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,40 +1,40 @@
+/**
+ * main class - use case palindrome app
+ *
+ * use case 2: hardcoded Palindrome verification
+ * description
+ * this class demonstrate basic palindrome validation using a hardcoded string value
+ *
+ * the application:
+ * stores a predefined string
+ * compares characters from both sides
+ * determine whether the string is palindrome or not
+ * display the result in the console
+ *
+ * this demonstrate the fundamental comparison logic before using advanced data structures
+ *
+ *
+ *
+ * @author  Nikhil Kumar
+ * @version 2.0
+ */
 public class PalindromeCheckerApp {
-    /**
-     * ============================================================
-     * MAIN CLASS - UseCase1PalindromeApp
-     * ============================================================
-     *
-     * Use Case 1: Application Entry & Welcome Message
-     *
-     * Description:
-     * This class represents the entry point of the
-     * Palindrome Checker Management System.
-     *
-     * At this stage, the application:
-     * - Starts execution from the main() method
-     * - Displays a welcome message
-     * - Shows application version
-     *
-     * No palindrome logic is implemented yet.
-     *
-     * The goal is to establish a clear startup flow.
-     *
-     * @author Developer
-     * @version 1.0
-     */
-
-    /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
-     *
-     * @param args Command-line arguments
-     */
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System.");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+    public static void main(String[] args){
+        //Hardcoded string
+        String word ="Nikhil";
+        boolean isPalindrome=true;
+        for(int i=0;i<word.length()/2;i++){
+            if(word.charAt(i)!=word.charAt(word.length()-i-1)){
+                isPalindrome=true;
+            }
+        }
+        System.out.println("Input text: Nikhil");
+        System.out.print("is it palindrome?:");
+        if (isPalindrome){
+            System.out.println("true");
+        }
+        else
+            System.out.println("false");
     }
+
 }
